37edd4622c2c06ca7e1f26cfc023432e86ca8aa9
Olga Komar <komar_ol@mail.ru>
2023-04-15 17:34:19 +0300

    added the solution to the second task

--- Program.cs
+++ Program.cs
@@ -3,3 +3,26 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+
+int NumberEntry()
+{
+    Console.Write("Введите трехзначное число= ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ThirdDigit(int number)
+{
+    number = number / 100;
+    if (number == 0)
+    {
+        Console.WriteLine("Нет третей цифры");
+    }
+    else
+    {
+        number = number % 10;
+        Console.WriteLine($"Третяя цифра = {number}");
+    }
+}
+
+int num = NumberEntry();
+ThirdDigit(num);
